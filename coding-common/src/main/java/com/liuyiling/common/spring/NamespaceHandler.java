a31f736c32e8d9949ad326f25f0c07886cdd5eab
lyl3@meitu.com <lyl3@meitu.com>
2017-01-01 09:40:15 +0800

    提交coding.xsd配置,调试Bug

--- coding-common/src/main/java/com/liuyiling/common/spring/NamespaceHandler.java
+++ coding-common/src/main/java/com/liuyiling/common/spring/NamespaceHandler.java
@@ -2,7 +2,7 @@ package com.liuyiling.common.spring;
 
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
-public class MeituNamespaceHandler extends NamespaceHandlerSupport {
+public class NamespaceHandler extends NamespaceHandlerSupport {
 
 	/* (non-Javadoc)
 	 * @see org.springframework.beans.factory.xml.NamespaceHandler#init()
